8c63c6b82aa7008ec696377445dfdf9eeae05075
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-07-09 08:17:21 +0200

    ci: better release github action

--- release.sh
+++ release.sh
@@ -5,9 +5,6 @@
 # goto git root
 cd "$(git rev-parse --show-toplevel)" || return 1
 
-#───────────────────────────────────────────────────────────────────────────────
-# BUMP VERSION NUMBER
-
 # Prompt for next version number
 nextVersion="$*"
 currentVersion=$(plutil -extract version xml1 -o - info.plist | sed -n 4p | cut -d">" -f2 | cut -d"<" -f1)
@@ -23,37 +20,11 @@ if [[ -f "$localInfoPlist" ]]; then
 	plutil -replace version -string "$nextVersion" "$localInfoPlist"
 fi
 
-#───────────────────────────────────────────────────────────────────────────────
-# COMPILE ALFREDWORKFLOW FILE
-
-# backup info.plist
-cp -v info.plist info-original.plist # backup
-
-# remove variables flagged as "no export" from info.plist
-if plutil -extract variablesdontexport json -o - info.plist &>/dev/null; then
-	excludedVars=$(plutil -extract variablesdontexport json -o - info.plist | tr -d "[]\"" | tr "," "\n")
-	echo "$excludedVars" | tr "\n" "\0" | xargs -0 -I {} plutil -replace variables.{} -string "" info.plist
-fi
-
-# remove workflow file from previous release
-rm -fv ./*.alfredworkflow
-
-# zip
-workflowName=$(plutil -extract name xml1 -o - info.plist | sed -n 4p | cut -d">" -f2 | cut -d"<" -f1 | tr " " "-")
-zip --quiet --recurse-paths "$workflowName.alfredworkflow" . \
-	--exclude ".git*" "info-original.plist" "prefs.plist" "*.alfredworkflow"
-
-# restore original
-rm -fv info.plist && mv -fv info-original.plist info.plist
-
-#───────────────────────────────────────────────────────────────────────────────
 # GIT OPERATIONS
-
 git add -A
 git commit -m "release $nextVersion"
 git pull
 git push
 
-# trigger the release action via github action
-git tag "$nextVersion"
+git tag "$nextVersion" # trigger the release action via github action
 git push origin --tags
