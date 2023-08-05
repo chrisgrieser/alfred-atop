023a10a3b5cc0a5d42edaa32de3e9dd27e248943
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-08-05 11:15:44 +0200

    improv: indicator of switched dns

--- scripts/select-dns-server.js
+++ scripts/select-dns-server.js
@@ -8,7 +8,6 @@ app.includeStandardAdditions = true;
 /** @type {AlfredRun} */
 // rome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
-	const currentDns = app.doShellScript("networksetup -getdnsservers Ethernet").split("\r")[0];
 
 	/** @type AlfredItem[] */
 	const selectableDns = [
@@ -16,13 +15,17 @@ function run() {
 			title: "Google",
 			subtitle: "8.8.8.8  &  8.8.4.4",
 			arg: ["8.8.8.8", "8.8.4.4"],
+			variables: { server_name: "Google" },
 		},
 		{
 			title: "Cloudflare",
 			subtitle: "1.1.1.1  &  1.0.0.1",
 			arg: ["1.1.1.1", "1.0.0.1"],
+			variables: { server_name: "Cloudflare" },
 		},
 	];
+
+	const currentDns = app.doShellScript("networksetup -getdnsservers Ethernet").split("\r")[0];
 	if (currentDns === "8.8.8.8") selectableDns[0].title = "✅ Google";
 	else if (currentDns === "1.1.1.1") selectableDns[1].title = "✅ Cloudflare";
 
