7d4c16751ea9803717a7ac11d1fd55b64e90e8a9
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-12-17 15:43:11 +0100

    fix(volumes): ignore special 'Recovery' volume

--- scripts/volume-search.js
+++ scripts/volume-search.js
@@ -3,6 +3,9 @@ const app = Application.currentApplication();
 app.includeStandardAdditions = true;
 //──────────────────────────────────────────────────────────────────────────────
 
+// special volumes to ignore
+const ignoreVolumes = ["Recovery"];
+
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run() {
 	let rerunSecs = 4; // slow reruns as default
@@ -11,12 +14,12 @@ function run() {
 	const volumes = app
 		.doShellScript("df -HY") // -H: human readable sizes (base10), -Y: filesystem format
 		.split("\r")
-		.filter((line) => line.includes(" /Volumes/"))
-		.map((vol) => {
+		.reduce((/** @type {AlfredItem[]} */ volumes, line) => {
+			if (!line.includes(" /Volumes/")) return volumes;
 			// quicker reruns when volume stats unavailable
-			if (vol.includes("unavailable")) rerunSecs = 0.5;
+			if (line.includes("unavailable")) rerunSecs = 0.5;
 
-			const info = vol
+			const info = line
 				.split(/\s+/)
 				.map((value) => value.replaceAll("unavailable", "…").replace(/(\d[GTMk]$)/, "$1b"));
 
@@ -24,14 +27,17 @@ function run() {
 			const path = info.slice(9).join(" ");
 			const name = path.replace("/Volumes/", "");
 
+			if (ignoreVolumes.includes(name)) return volumes;
+
 			const subtitle = `『${format}』   Total: ${total}   Available: ${available}   Used: ${used} (${share})`;
-			return {
+			volumes.push({
 				title: name,
 				uid: path, // during rerun remembers selection, but does not affect sorting
 				subtitle: subtitle,
 				arg: path,
-			};
-		});
+			});
+			return volumes;
+		}, []);
 
 	// No Volume found
 	if (volumes.length === 0) {
