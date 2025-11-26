e04c940ffc0dee1f6a5d72a6e30750c628fb1bba
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-11-26 17:27:45 +0100

    fix: Bluetooth devices search for macOS Tahoe (#19)

--- scripts/bluetooth-connect.js
+++ scripts/bluetooth-connect.js
@@ -8,27 +8,18 @@ app.includeStandardAdditions = true;
 /** @type {AlfredRun} */
 // biome-ignore lint/correctness/noUnusedVariables: Alfred run
 function run(argv) {
-	const selectedDeviceAddress = argv[0];
-
 	// DOCS https://developer.apple.com/documentation/iobluetooth/iobluetoothdevice
-	const devicesRaw = ObjC.unwrap($.IOBluetoothDevice.pairedDevices);
-	const allDevices = Array.from(devicesRaw, (d) => {
-		return {
-			name: ObjC.unwrap(d.nameOrAddress),
-			connected: ObjC.unwrap(d.isConnected),
-			address: ObjC.unwrap(d.addressString),
-			instance: d,
-		};
-	});
-
-	const device = allDevices.find((d) => d.address === selectedDeviceAddress);
+	const selectedDeviceAddress = argv[0];
+	/** @type {BluetoothDevice[]} */
+	const devices = $.IOBluetoothDevice.pairedDevices.js;
+	const device = devices.find((d) => d.addressString.js === selectedDeviceAddress);
 	if (!device) return "⚠️ Unknown error.";
 
-	// SIC function call without `()`, due to being Objective-C
-	if (device.connected) device.instance.closeConnection;
-	else device.instance.openConnection;
+	// SIC function call without `()`, due to being Objective-C?
+	device.isConnected ? device.closeConnection : device.openConnection;
 
-	const action = device.connected ? "🔴 Disconnected from" : "🟢 Connected to";
-	const alfredNotifMsg = `${action} "${device.name}"`;
+	// notification
+	const action = device.isConnected ? "🔴 Disconnected from" : "🟢 Connected to";
+	const alfredNotifMsg = `${action} "${device.nameOrAddress.js}"`;
 	return alfredNotifMsg;
 }
